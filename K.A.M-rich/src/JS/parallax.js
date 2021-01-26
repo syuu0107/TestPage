71da9875d1289ba9f47c99c40df3c91031e6dc77
syuu0107 <ishimura.0107.shuto@gmail.com>
2021-01-26 09:48:13 +0900

    update

--- K.A.M-rich/src/JS/parallax.js
+++ K.A.M-rich/src/JS/parallax.js
@@ -1,16 +1,16 @@
 $(function () {
   //各背景画像のtopの位置を取得
-  var bg1_top = $(".bg_1").offset().top;
-  var bg2_top = $(".bg_2").offset().top;
-  var bg3_top = $(".bg_3").offset().top;
+  var shop_1top = $(".shop-1").offset().top;
+  var shop_2top = $(".shop-2").offset().top;
+  var shop_3top = $(".shop-3").offset().top;
 
   // ウィンドウの高さを取得
   var win_h = $(window).height();
 
   //各背景画像を動かすタイミングの位置を計算
-  var start_bg1 = bg1_top - win_h;
-  var start_bg2 = bg2_top - win_h;
-  var start_bg3 = bg3_top - win_h;
+  var start_shop_1 = shop_1top - win_h;
+  var start_shop_2 = shop_2top - win_h;
+  var start_shop_3 = shop_3top - win_h;
 
 
   $(window).scroll(function () {
@@ -19,14 +19,14 @@ $(function () {
     var y = $(this).scrollTop();
 
     //スクロール量と動かすタイミング位置を判定した場合は背景画像を動かす
-    if (y >= start_bg1) {
-      $('.bg_1').css('background-position-y', -(y - bg1_top) * 0.2 + 'px');
+    if (y >= start_shop_1) {
+      $('.shop-1').css('background-position-y', -(y - shop_1top) * 0.2 + 'px');
     }
-    if (y > start_bg2) {
-      $('.bg_2').css('background-position-y', -(y - bg2_top) * 0.2 + 'px');
+    if (y > start_shop_2) {
+      $('.shop-2').css('background-position-y', -(y - shop_2top) * 0.2 + 'px');
     }
-    if (y > start_bg3) {
-      $('.bg_3').css('background-position-y', -(y - bg3_top) * 0.2 + 'px');
+    if (y > start_shop_3) {
+      $('.shop-3').css('background-position-y', -(y - shop_3top) * 0.2 + 'px');
     }
   });
 });
